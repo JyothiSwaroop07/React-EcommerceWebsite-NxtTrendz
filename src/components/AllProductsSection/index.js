8577dd162a9c7abf34645ab2c7ed38c327c73111
JyothiSwaroop07 <vv5382@srmist.edu.in>
2023-02-26 16:23:01 +0530

    filters added

--- src/components/AllProductsSection/index.js
+++ src/components/AllProductsSection/index.js
@@ -2,9 +2,34 @@ import {Component} from 'react'
 import Loader from 'react-loader-spinner'
 import Cookies from 'js-cookie'
 
-import ProductsHeader from '../ProductsHeader'
+import FiltersGroup from '../FiltersGroup'
 import ProductCard from '../ProductCard'
+import ProductsHeader from '../ProductsHeader'
+
 import './index.css'
+
+const categoryOptions = [
+  {
+    name: 'Clothing',
+    categoryId: '1',
+  },
+  {
+    name: 'Electronics',
+    categoryId: '2',
+  },
+  {
+    name: 'Appliances',
+    categoryId: '3',
+  },
+  {
+    name: 'Grocery',
+    categoryId: '4',
+  },
+  {
+    name: 'Toys',
+    categoryId: '5',
+  },
+]
 
 const sortbyOptions = [
   {
@@ -17,28 +42,78 @@ const sortbyOptions = [
   },
 ]
 
+const ratingsList = [
+  {
+    ratingId: '4',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rating-four-stars-img.png',
+  },
+  {
+    ratingId: '3',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rating-three-stars-img.png',
+  },
+  {
+    ratingId: '2',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rating-two-stars-img.png',
+  },
+  {
+    ratingId: '1',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rating-one-star-img.png',
+  },
+]
+
 class AllProductsSection extends Component {
   state = {
     productsList: [],
     isLoading: false,
     activeOptionId: sortbyOptions[0].optionId,
+    titleSearch: '',
+    category: '',
+    rating: '',
+    errorMsg: false,
   }
 
   componentDidMount() {
     this.getProducts()
   }
 
-  updateActiveOptionId = activeOptionId => {
-    this.setState({activeOptionId}, this.getProducts)
+  searchDataBase = () => {
+    this.getProducts()
+  }
+
+  changeInput = value => this.setState({titleSearch: value})
+
+  filterClick = value => {
+    this.setState({category: value}, this.getProducts)
+  }
+
+  ratingClick = value => {
+    this.setState({rating: value}, this.getProducts)
+  }
+
+  clearFilters = () => {
+    this.setState(
+      {
+        titleSearch: '',
+        category: '',
+        rating: '',
+        activeOptionId: sortbyOptions[0].optionId,
+      },
+      this.getProducts,
+    )
   }
 
   getProducts = async () => {
-    const {activeOptionId} = this.state
     this.setState({
       isLoading: true,
     })
     const jwtToken = Cookies.get('jwt_token')
-    const apiUrl = `https://apis.ccbp.in/products?sort_by=${activeOptionId}`
+
+    const {activeOptionId, titleSearch, category, rating} = this.state
+    const apiUrl = `https://apis.ccbp.in/products?sort_by=${activeOptionId}&title_search=${titleSearch}&category=${category}&rating=${rating}`
     const options = {
       headers: {
         Authorization: `Bearer ${jwtToken}`,
@@ -59,24 +134,65 @@ class AllProductsSection extends Component {
       this.setState({
         productsList: updatedData,
         isLoading: false,
+        errorMsg: false,
       })
+    } else {
+      this.setState({errorMsg: true, isLoading: false})
     }
   }
 
+  changeSortby = activeOptionId => {
+    this.setState({activeOptionId}, this.getProducts)
+  }
+
+  failureView = () => (
+    <div className="failure-view">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz/nxt-trendz-products-error-view.png"
+        className="failure-img"
+        alt="products failure"
+      />
+      <h1 className="failure-heading">Oops! Something Went Wrong</h1>
+      <p className="failure-para">
+        We are having some trouble processing your request. Please try again.
+      </p>
+    </div>
+  )
+
   renderProductsList = () => {
-    const {productsList, activeOptionId} = this.state
+    const {productsList, activeOptionId, errorMsg} = this.state
+
+    if (errorMsg) {
+      return this.failureView()
+    }
     return (
       <>
-        <ProductsHeader
-          sortbyOptions={sortbyOptions}
-          activeOptionId={activeOptionId}
-          updateActiveOptionId={this.updateActiveOptionId}
-        />
-        <ul className="products-list">
-          {productsList.map(product => (
-            <ProductCard productData={product} key={product.id} />
-          ))}
-        </ul>
+        {productsList.length === 0 ? (
+          <div className="no-products">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz/nxt-trendz-no-products-view.png"
+              className="no-products-img"
+              alt="no products"
+            />
+            <h1 className="no-products-found">No Products Found</h1>
+            <p className="no-products-line">
+              We could not find any products. Try other filters.
+            </p>
+          </div>
+        ) : (
+          <div className="all-products-container">
+            <ProductsHeader
+              activeOptionId={activeOptionId}
+              sortbyOptions={sortbyOptions}
+              changeSortby={this.changeSortby}
+            />
+            <ul className="products-list">
+              {productsList.map(product => (
+                <ProductCard productData={product} key={product.id} />
+              ))}
+            </ul>
+          </div>
+        )}
       </>
     )
   }
@@ -88,8 +204,27 @@ class AllProductsSection extends Component {
   )
 
   render() {
-    const {isLoading} = this.state
-    return isLoading ? this.renderLoader() : this.renderProductsList()
+    const {isLoading, category, rating, titleSearch} = this.state
+
+    return (
+      <div className="all-products-section">
+        {/* TODO: Update the below element */}
+        <FiltersGroup
+          categoryOptions={categoryOptions}
+          ratingsList={ratingsList}
+          searchDataBase={this.searchDataBase}
+          filterClick={this.filterClick}
+          ratingClick={this.ratingClick}
+          clearFilters={this.clearFilters}
+          changeInput={this.changeInput}
+          category={category}
+          rating={rating}
+          titleSearch={titleSearch}
+        />
+
+        {isLoading ? this.renderLoader() : this.renderProductsList()}
+      </div>
+    )
   }
 }
 
